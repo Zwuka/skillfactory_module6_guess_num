113388eacd930feadc96ea37b68417b194b3ac80
Evgeniy-Kuryan <e.kurian83@gmail.com>
2022-01-11 18:13:12 +0300

    Version for submission

--- game_v2.py
+++ game_v2.py
@@ -6,7 +6,10 @@ import numpy as np
 
 
 def random_predict(number: int = 1) -> int:
-    """Рандомно угадываем число
+    """Угадываем число с помощью бинарного поиска, каждый раз предполагая что искомое число среднее в группе.
+    Изначально группа определяется как нижняя граница -1 от минимально возможного числа, верхняя граница 
+    +1 от максимально возможного числа. Если после первой попытки загаданно число больше, то нижняя граница
+    становится числом из первой группы, если меньше, то этим числом становиться верхняя граница.
 
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
@@ -15,10 +18,17 @@ def random_predict(number: int = 1) -> int:
         int: Число попыток
     """
     count = 0
-
+    up_bond = 101
+    low_bond = 0
+    predict_number = up_bond//2
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
+        if predict_number > number:
+            up_bond = predict_number
+            predict_number = (up_bond+low_bond)//2
+        elif predict_number < number:
+            low_bond = predict_number
+            predict_number = (up_bond+low_bond)//2
         if number == predict_number:
             break  # выход из цикла если угадали
     return count
@@ -34,10 +44,11 @@ def score_game(random_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
+
         count_ls.append(random_predict(number))
 
     score = int(np.mean(count_ls))
@@ -47,4 +58,5 @@ def score_game(random_predict) -> int:
 
 if __name__ == "__main__":
     # RUN
-    score_game(random_predict)
+
+    print(score_game(random_predict))
